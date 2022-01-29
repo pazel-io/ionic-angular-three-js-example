68e2623f50b1cee2bf8686dbe4633af277c12be6
Parham <parham.fazel@spatialvision.com.au>
2022-01-29 22:41:18 +1100

    missing modal title

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,18 +12,20 @@ export class HomePage {
     toy: {
       fileName: '1643422158392.gltf',
       cssClass: 'cuddly-pink',
+      name: 'Snugglebug',
     },
     pot: {
       fileName: '1643422846453.gltf',
       position: [0, 0, 3],
       cssClass: 'dark-purple',
+      name: 'Mina Kari Sugar Cube Bowl',
     },
   };
 
   constructor(private modalController: ModalController) {}
 
-  async view(name: string) {
-    const { fileName, position, cssClass } = this.models[name];
+  async view(key: string) {
+    const { fileName, position, cssClass, name } = this.models[key];
     const modal = await this.modalController.create({
       component: ViewerComponent,
       cssClass: 'my-custom-class',
@@ -31,9 +33,10 @@ export class HomePage {
       initialBreakpoint: 0.95,
       componentProps: {
         fileName,
-        filePath: `/assets/models/${name}/`,
+        filePath: `/assets/models/${key}/`,
         position,
         cssClass,
+        name,
       },
     });
     return await modal.present();
